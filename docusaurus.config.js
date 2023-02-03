50844b3b732d4df9b75a4e5b978baf5efe4864d4
fenghang <hang.feng@nx-engine.com>
2023-02-03 13:22:32 +0800

    feat:修改title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'ffff',
+  title: 'Feng Hang',
   tagline: 'Dinosaurs are cool',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
